aac38d88cc5a7d589c2de398fa7e135ce8b144ae
zhaoqi.15 <zhaoqi.15@bytedance.com>
2024-04-12 11:56:14 +0800

    feat: 部分配置化

---
+++ src/types.js
@@ -0,0 +1,10 @@
+export const OPEN_KEY = '__isOpenInspect__'
+
+const ROOT_DIR_KEY = 'src'
+
+const IGNORE_DIR_KEY = 'node_modules'
+
+export const defaultOpt = {
+  rootDirKey: ROOT_DIR_KEY,
+  ignoreDirKey: IGNORE_DIR_KEY,
+}
